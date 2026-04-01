720dbb44e06923abc06bd5ae2e2ae7e892f29e26
rishnik4432 <rishnik2019@gmail.com>
2026-04-01 17:07:55 +1100

    Initial commit

---
+++ app/src/main/java/com/example/task41p/Question.java
@@ -0,0 +1,16 @@
+package com.example.task41p;
+
+public class Question {
+    private String text;
+    private String[] options;
+    private int correctOptionIndex;
+
+    public Question(String text, String[] options, int correctOptionIndex) {
+        this.text = text;
+        this.options = options;
+        this.correctOptionIndex = correctOptionIndex;
+    }
+    public String getText() { return text;}
+    public String[] getOptions() { return options;}
+    public int getCorrectOptionIndex() { return correctOptionIndex;}
+}
